4fb5444e2bf0bb13875a25b8ffead8326bf6d1d6
rn-consider <3214858229@qq.com>
2025-03-05 17:25:33 +0800

    t11

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-let assetPrefix = `rn-consider.github.io`;
 // 用于为应用设置基础路径。这在将应用部署到子目录下时特别有用，因为它允许您指定应用所在的目录
 let basePath = `/rn-consider.github.io`;
 const nextConfig = {
